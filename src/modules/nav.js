6671a7783b14590c888442905097b8ed5ee3f246
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-06-16 15:42:30 +0100

    added the swichView effect in nav.js

---
+++ src/modules/nav.js
@@ -0,0 +1,7 @@
+import { DateTime } from './luxon.js';
+
+const listNav = document.querySelectorAll('.listNav');
+const timeZone = document.getElementById('#times-zone');
+const switchViews = (sectionDisplayed) => {
+    const sectionDisplayed =document.querySelectorAll('.sectionDisplayed');
+}
