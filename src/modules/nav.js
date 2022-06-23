02d7701f13d48400b1bb3f68130bcd3fbbaeae8e
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-06-23 14:21:15 +0100

    modifing the evenListener of link list

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -1,7 +1,1 @@
-import { DateTime } from './luxon.js';
-
-const listNav = document.querySelectorAll('.listNav');
-const timeZone = document.getElementById('#times-zone');
-const switchViews = (sectionDisplayed) => {
-    const sectionDisplayed =document.querySelectorAll('.sectionDisplayed');
-}
+const navLinks = document.querySelectorAll('nav');
