968fee9909dce0ce8fc178a5dfd08d84020f0010
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-08-05 17:27:28 +0100

    added the functionality at 45% in app.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const navigator = document.querySelectorAll('.listNav');
 const mainContent = document.querySelector('.main-Content');
 const addBook = document.querySelector('.addBookForm');
 const contactinfo = document.querySelector('.contact');
+// navigation in the page
 navigator.forEach(element => {
     element.addEventListener('click',()=>{
         if(element.id == 'list'){
@@ -16,9 +17,58 @@ navigator.forEach(element => {
             contactinfo.classList.add('contactActive');
             mainContent.classList.remove('main-contentActive');
             addBook.classList.remove('addBookFormActive');
-        }else{
-            console.log('ces pas les lol');
         }
     });
 });
 window.onload = mainContent.classList.add('main-contentActive');
+// navigation in the page
+
+//data for the element
+const data =[
+    {
+        Title:'Sunshine',
+        Author:'soulemanou kunchiefuh'
+    },
+    {
+    
+        Title:'Google',
+        Author:'User Account'
+    },
+    {
+        Title:'Soodely',
+        Author:'Babanou '
+    },
+    {
+        Title:'Live trails',
+        Author:'davide living stone'
+    },
+    {
+        Title:'Bloque',
+        Author:'Fally Ipupa'
+    },
+   
+];
+function display(data){
+    const bookItem = document.querySelector('.bookItem');
+    for(let i=0; i<= data.length; ++i){
+        const div = document.createElement('div');
+        div.className = 'bookItem';
+        // div.className = 'indexBookItem';
+        const div2 = document.createElement('div');
+        div2.className = 'about-Book';
+        div2.innerHTML =`<q>${data[i].Title}</q> 
+        &nbsp; By &nbsp;
+        <span>${data[i].Author}</span>`;     
+        const btnEl = document.createElement('button');
+        btnEl.className = 'btnRemove';
+        btnEl.innerHTML = 'Remove'
+        div.appendChild(div2);
+        div.appendChild(btnEl);
+        mainContent.appendChild(div);
+        console.log(i);
+    };
+    // console.log(arrElement);
+};
+
+display(data);
+// 
